c2e5eda15ec2ee710e68e57e45b22aae211ce21e
ashin1234 <78942006+ashin1234@users.noreply.github.com>
2022-01-17 20:01:21 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
-array_1 =["cup","paper","teddybear","ice cream"];
+array_1 =["cup","paper","bear","ice_cream"];
 var timer_counter=0;
 var timer_check="";
 var drawn_sketch="";
 var sketch="";
 var answer_holder="";
-var score=0;
+var score=0; 
 
 function setup(){
     canvas = createCanvas(280, 280);
     canvas.center();
     background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesis;
 } 
+
+function preload(){
+    classifier = ml5.imageClassifier('DoodleNet');
+}
 function updateCanvas(){
     background("white");
     random_number=Math.floor((Math.random()*array_1.length)+1);
@@ -33,11 +39,30 @@ function check_sketch(){
     }
 }
 function draw(){
+    strokeWeight(13);
+    stroke(0);
+    if(mouseIsPressed) {
+        line(pmouseX, pmouseY, mouseX, mouseY);
+    }
  check_sketch();
  if(drawn_sketch==sketch){
      answer_holder="set";
      score++;
-     document.getElementById("score").innerHTML="score: "+score;
-
+     document.getElementById("score").innerHTML="score: "+score;   
  }
+
+}
+function classifyCanvas(){
+    classifier.classify(canvas, gotResult);
+}
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+    drawn_sketch = results[0].label;
+    document.getElementById('label').innerHTML = 'label: ' + results[0].label;
+    document.getElementById('confidence').innerHTML = 'Confidence: ' + Math.round(results[0].confidence * 100) + '%';
+    utterThis = new SpeechSynthesisUtterance(results[0].label);
+    synth.speak(utterThis);
 }
